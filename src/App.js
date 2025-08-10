f5324caa24eece840bdbe53fb7de444da0d14b53
jujak1105-crypto <jujak1105@gmail.com>
2025-08-10 14:23:09 +0900

    초기세팅

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div id="layout">
+      <header id="header">header</header>
+      <main id="main">main</main>
+      <footer id="footer">footer</footer>
+    </div>
+  );
+}
+
+export default App;
